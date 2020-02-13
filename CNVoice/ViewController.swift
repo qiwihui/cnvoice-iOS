a1abd8a662991c39761d777141fee478a9ab7dd9
qiwihui <qwh005007@gmail.com>
2020-02-13 14:33:01 +0800

    Initial Commit

---
+++ CNVoice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CNVoice
+//
+//  Created by qiwihui on 2/13/20.
+//  Copyright © 2020 qiwihui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
